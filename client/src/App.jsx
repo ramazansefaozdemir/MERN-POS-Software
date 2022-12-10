a44260fc9efcd156399517e2d13ad134be09bc46
Ramazan <ramazanozmr@gmail.com>
2022-12-10 22:31:47 +0300

    categories component added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
+import React from 'react';
+import Categories from './components/categories/Categories';
 import Header from './components/header/Header'
 
 function App() {
   return (
-   <div>
+   <React.Fragment>
      <Header />
-     home
-   </div>
+     <div className="home px-6 flex justify-between gap-10">
+      <div className="categories flex-1 overflow-auto max-h-[calc(100vh-_-100px)] pb-72">
+        <Categories />
+      </div>
+      <div className="products flex-[8]">
+        <div>products</div>
+      </div>
+      <div className="card-totals">
+        <div>card totals</div>
+      </div>
+     </div>
+   </React.Fragment>
   );
 }
 
