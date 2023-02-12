d6149bf79101a37ac619ebb27885dbd61e9954c9
Ramazan <ramazanozmr@gmail.com>
2023-02-12 14:41:29 +0300

    Added filtering to tables

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -1,16 +1,125 @@
-import { Card, Table, Button, message, Popconfirm } from "antd"
-import { useState } from "react";
+import { Card, Table, Button, message, Popconfirm, Input, Space } from "antd"
+import { SearchOutlined } from '@ant-design/icons';
+import { useRef, useState } from "react";
 import Header from "../components/header/Header"
 import CreateBill from "./CreateBill";
 import { useSelector,useDispatch } from "react-redux";
 import { PlusCircleOutlined, MinusCircleOutlined } from '@ant-design/icons';
 import { increase, decrease, deleteCart } from '../redux/cartSlice.js';
 import { formatPrice } from "../utils/formatPrice";
+import Highlighter from 'react-highlight-words';
 
 const CartPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const cart = useSelector((state) => state.cart)
   const dispatch = useDispatch();
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef(null);
+
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText('');
+  };
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+        onKeyDown={(e) => e.stopPropagation()}
+      >
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: 'block',
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Ara
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Sil
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({
+                closeDropdown: false,
+              });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filtrele
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            Kapat
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? '#1890ff' : undefined,
+        }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{
+            backgroundColor: '#ffc069',
+            padding: 0,
+          }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
     
   const columns = [
     {
@@ -26,16 +135,19 @@ const CartPage = () => {
       title: 'Ürün Adı',
       dataIndex: 'title',
       key: 'title',
+      ...getColumnSearchProps("title"),
     },
     {
       title: 'Kategori',
       dataIndex: 'category',
       key: 'category',
+      ...getColumnSearchProps("category"),
     },
     {
       title: 'Ürün Fiyatı',
       dataIndex: 'price',
       key: 'price',
+      sorter: (a,b) => a.price - b.price,
       render: (text) => {
         return (<span>{formatPrice(text)}</span>)
       }
--- client/src/pages/CustomerPage.jsx
+++ client/src/pages/CustomerPage.jsx
@@ -1,9 +1,14 @@
-import { Table } from 'antd';
-import { useEffect, useState } from 'react'
+import { Button, Input, Space, Table } from 'antd';
+import { useEffect, useRef, useState } from 'react'
 import Header from '../components/header/Header'
+import Highlighter from 'react-highlight-words';
+import { SearchOutlined } from '@ant-design/icons';
 
 const CustomerPage = () => {
   const [billItems, setBillItems] = useState();
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef(null);
   
   useEffect(()=>{
     try {
@@ -17,17 +22,123 @@ const CustomerPage = () => {
       console.log(error);
     }
   }, [])
+
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText('');
+  };
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+        onKeyDown={(e) => e.stopPropagation()}
+      >
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: 'block',
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Ara
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Sil
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({
+                closeDropdown: false,
+              });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filtrele
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            Kapat
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? '#1890ff' : undefined,
+        }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{
+            backgroundColor: '#ffc069',
+            padding: 0,
+          }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
       
   const columns = [
     {
       title: 'Müşteri Adı',
       dataIndex: 'customerName',
       key: 'customerName',
+      ...getColumnSearchProps("customerName")
     },
     {
       title: 'Telefon Numarası',
       dataIndex: 'customerPhoneNumber',
       key: 'customerPhoneNumber',
+      ...getColumnSearchProps("customerPhoneNumber")
     },
     {
       title: 'İşlem Tarihi',
